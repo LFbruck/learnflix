42ca97e7e181c7eb0096dfd26e5d5898aa588f79
Luiz Filipe Bruck Reis Mattos <lfbruckmattos@gmail.com>
2026-01-27 15:32:43 -0300

    feat: Formulario Upload de Materiais

--- src/components/AdicionarMateriais/AdicionarMateriais.jsx
+++ src/components/AdicionarMateriais/AdicionarMateriais.jsx
@@ -8,9 +8,25 @@ export default function AdicionarMateriais() {
 
     const [descricao, setDescricao] = useState("");
 
-    const [arquivo, setArquivo] = useState("");
+    const [arquivo, setArquivo] = useState(null);
 
     const [loading, setLoading] = useState(false);
 
-    return ("");
+    const handleSubmit = (e) => {
+        e.preventDefault();
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <label>Titulo: </label>
+            <input type="text" value={titulo} placeholder= "Titulo do Material" onChange={(e) => setTitulo(e.target.value)} required={true} />
+            <label>Descrição: </label>
+            <textarea  value={descricao} placeholder= "Descrição do Conteudo" onChange={(e) => setDescricao(e.target.value)}  />
+            <label>Upload Arquivo: </label>
+            <input type="file" onChange={(e) => setArquivo(e.target.files[0]) } accept=".jpg,.png, .pdf, .doc, .docx" required={true} /><button type="submit" disabled={loading}>
+            {loading ? 'Carregando...' : 'Adicionar Material'}
+        </button>
+        </form>
+
+    );
 }
