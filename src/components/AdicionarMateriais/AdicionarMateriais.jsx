38ea67b99ff9e55d1b513d343203aca08c3c900b
Luiz Filipe Bruck Reis Mattos <lfbruckmattos@gmail.com>
2026-01-27 15:19:31 -0300

    feat: Adicionar Materiais

--- src/components/AdicionarMateriais/AdicionarMateriais.jsx
+++ src/components/AdicionarMateriais/AdicionarMateriais.jsx
@@ -1,6 +1,16 @@
 import styles from "./AdicionarMateriais.module.css"
+import {useState} from "react";
 
 
 export default function AdicionarMateriais() {
+
+    const [titulo, setTitulo] = useState("");
+
+    const [descricao, setDescricao] = useState("");
+
+    const [arquivo, setArquivo] = useState("");
+
+    const [loading, setLoading] = useState(false);
+
     return ("");
 }
--- src/components/PaginaMateriais/PaginaMateriais.jsx
+++ src/components/PaginaMateriais/PaginaMateriais.jsx
@@ -5,12 +5,12 @@ import {useState, useEffect} from 'react'
 
 export default function PaginaMateriais() {
     const [materiais, setMateriais] = useState([
-        {titulo: 'Fundamentos do React', descricao: "Aula de Fundamento do React", nomeArquivo: "Aula2-Fundamento do React"},
+        {titulo: 'Fundamentos do React', descricao: "Aula de Fundamento do React", nomeArquivo: "Aula2-Fundamento do React", id: 1, tipoArquivo: "PDF"},
     ])
 
     return (
         <div className={styles.container}>
-            {}
+
         </div>
     )
 
