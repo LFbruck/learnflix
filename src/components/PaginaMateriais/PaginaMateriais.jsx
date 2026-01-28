0d84890853326488452c180dc478bee5120033db
Luiz Filipe Bruck Reis Mattos <lfbruckmattos@gmail.com>
2026-01-28 15:53:02 -0300

    Feat: Funções Construidas

--- src/components/PaginaMateriais/PaginaMateriais.jsx
+++ src/components/PaginaMateriais/PaginaMateriais.jsx
@@ -6,10 +6,19 @@ import {useState, useEffect} from 'react'
 export default function PaginaMateriais() {
     const [materiais, setMateriais] = useState([
         {titulo: 'Fundamentos do React', descricao: "Aula de Fundamento do React", nomeArquivo: "Aula2-Fundamento do React", id: 1, tipoArquivo: "PDF"},
-    ])
+    ]);
+
+    const adicionarMaterial = (novoMaterial) => {
+        setMateriais([...materiais, novoMaterial]);
+    }
+
+    const excluirMaterial = (id) => {
+        setMateriais(materiais.filter(material=> material.id !== id));
+    }
 
     return (
         <div className={styles.container}>
+
 
         </div>
     )
