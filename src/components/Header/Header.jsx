e8dca81f23c8f10dd5d47e2b8d12ac4025a65aa3
Gustavo Mendes de Oliveira <167227958+gustavomoliveira@users.noreply.github.com>
2026-01-29 16:59:51 -0300

    feat(header): adiciona menu sanduíche para mobile e tablet

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,23 @@
 import { Link } from "react-router-dom";
 import styles from "./Header.module.css";
+import { useState } from "react";
+import { HiMenu } from "react-icons/hi";
 
 export default function Header() {
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen);
+    }
+
     return (
         <header className={styles.header}>
             <div className={styles.container}>
                 <h1 className={styles.title}>LearnFlix</h1>
-                <nav className={styles.nav}>
+                <button className={styles.menuButton} onClick={toggleMenu} aria-label={"Menu"}>
+                    <HiMenu />
+                </button>
+                <nav className={menuOpen ? `${styles.nav} ${styles.navOpen}` : styles.nav}>
                     <Link to={"/"} className={styles.link}>
                         HomePage
                     </Link>
