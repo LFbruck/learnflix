c43ced3bb371d1339387c8b5223ed812d3e2c66d
Luiz Filipe Bruck Reis Mattos <lfbruckmattos@gmail.com>
2026-02-09 22:56:01 -0300

    Feat: VisualizarPage

---
+++ src/pages/VisualizarPage/VisualizarPage.jsx
@@ -0,0 +1,29 @@
+import styles from "./VisualizarPage.module.scss";
+import dados from "../../data/materiais.json";
+
+export default function VisualizarPage() {
+    const disciplinas = dados.disciplinas;
+
+    return (
+        <div className={styles.container}>
+            <h1 className={styles.titulo}>Visualizar Disciplinas</h1>
+
+            <div className={styles.lista}>
+                {disciplinas.map((disciplina) => (
+                    <div key={disciplina.id} className={styles.card}>
+                        <h2>{disciplina.nome}</h2>
+
+                        <p>
+                            {disciplina.materiais.length > 0 ?  `${disciplina.materiais.length} materiais disponíveis`
+                                : "Nenhum material disponível"}
+                        </p>
+
+                        <button className={styles.botao}>
+                            Acessar Materiais
+                        </button>
+                    </div>
+                ))}
+            </div>
+        </div>
+    )
+}
