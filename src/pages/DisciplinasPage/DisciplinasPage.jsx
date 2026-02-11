c229012645f92cd0524178c595838f06021f60b6
Gustavo Mendes de Oliveira <167227958+gustavomoliveira@users.noreply.github.com>
2026-02-11 17:07:05 -0300

    feat: implementa fetch de disciplinas do JSON

--- src/pages/DisciplinasPage/DisciplinasPage.jsx
+++ src/pages/DisciplinasPage/DisciplinasPage.jsx
@@ -1,6 +1,28 @@
 import styles from "./DisciplinasPage.module.css";
+import getDisciplinas from "../../services/disciplinasService";
+import { useEffect, useState } from "react";
 
 export default function DisciplinasPage() {
+    const [disciplinas, setDisciplinas] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    async function carregarDisciplinas() {
+        try {
+            const data = await getDisciplinas();
+            console.log("Dados carregados: ", data);
+            setDisciplinas(data.disciplinas);
+        } catch (error) {
+            setError(error.message);
+        } finally {
+            setLoading(false);
+        }
+    }
+
+    useEffect(() => {
+        carregarDisciplinas();
+    }, [])
+
     return (
         <div className={styles.container}>
             <div className={styles.nav}>
@@ -8,7 +30,7 @@ export default function DisciplinasPage() {
                 <button className={styles.btnCriarDisciplina}></button>
             </div>
             <div className={styles.gridCards}>
-                <CardDisciplina />
+                {/*<CardDisciplina />*/}
             </div>
         </div>
     );
