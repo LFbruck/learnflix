2b14b7006beedcfa0326da4e27f01c053425f6d6
Luiz Filipe Bruck Reis Mattos <lfbruckmattos@gmail.com>
2026-03-16 12:34:33 -0300

    Test 2: Botao muda para seu texto ao enviar

--- src/components/AdicionarMateriais/AdicionarMateriais.Test.jsx
+++ src/components/AdicionarMateriais/AdicionarMateriais.Test.jsx
@@ -10,15 +10,33 @@ describe("AdicionarMateriais", () => {
         jest.runOnlyPendingTimers();
         jest.useRealTimers();
     });
-
-    test("renderiza os campos do formulário e o botão", () => {
+    test("renderiza os campos do formulário e o botao", () => {
         render(<AdicionarMateriais adicionarMaterial={jest.fn()} />);
 
         expect(screen.getByLabelText(/titulo/i)).toBeInTheDocument();
         expect(screen.getByLabelText(/descrição/i)).toBeInTheDocument();
         expect(screen.getByLabelText(/upload arquivo/i)).toBeInTheDocument();
-
         expect(
             screen.getByRole("button", { name: /adicionar material/i })
+        ).toBeInTheDocument();
+    });
+    test("ao enviar, o botão muda para 'Carregando...'", () => {
+        render(<AdicionarMateriais adicionarMaterial={jest.fn()} />);
+        fireEvent.change(screen.getByLabelText(/titulo/i), {
+            target: { value: "Material Teste" },
+        });
+
+        fireEvent.change(screen.getByLabelText(/descrição/i), {
+            target: { value: "Descrição Teste" },
+        });
+        const arquivo = new File(["conteudo"], "react.pdf", {
+            type: "application/pdf",
+        });
+        const inputUpload = screen.getByLabelText(/upload arquivo/i);
+        fireEvent.change(inputUpload, { target: { files: [arquivo] } });
+
+        fireEvent.click(screen.getByRole("button", { name: /adicionar material/i }));
+        expect(
+            screen.getByRole("button", { name: /carregando/i })
         ).toBeInTheDocument();
     });
